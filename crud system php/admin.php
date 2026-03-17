7798ebdffc94b9983e004229ebcfd119104ff5b0
sayoridevNL <sayoridev2@gmail.com>
2026-03-17 11:31:45 +0100

    updated the index.php and the login.php
    I updated the login.php so there is rn styling and the option to register an account. I also created a possible homepage that can be changed

--- crud system php/admin.php
+++ crud system php/admin.php
@@ -2,10 +2,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "";
+$dbname = "users";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=,$dbname", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
